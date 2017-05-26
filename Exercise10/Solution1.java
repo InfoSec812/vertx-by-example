4348d3c5e73e813e133d099c32fe491870224cb5
Deven Phillips <deven.phillips@gmail.com>
2017-05-26 11:20:35 -0400

    Updated Exercise 10

--- Exercise10/Solution1.java
+++ Exercise10/Solution1.java
@@ -1,29 +1,31 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetServer;
+import io.vertx.core.net.NetServerOptions;
 import io.vertx.core.net.NetSocket;
 
 public class Solution1 extends AbstractVerticle {
 
     public void start() {
-        def opts = [
-            host: '0.0.0.0',
-            port: 1080,
-            logActivity: true
-        ]
+        JsonObject opts = new JsonObject()
+                .put("host", "0.0.0.0")
+                .put("port", 1080)
+                .put("logActivity", true);
 
-        NetServer server = vertx.createNetServer(opts)
-        server.connectHandler(this::connectHandler).listen()
-        LoggerFactory.getLogger(Solution1).info('Deployed NetServer Solution1')
+        NetServer server = vertx.createNetServer(new NetServerOptions(opts));
+        server.connectHandler(this::connectHandler).listen();
+        LoggerFactory.getLogger("Solution1").info("Deployed NetServer Solution1");
     }
 
     void connectHandler(NetSocket socket) {
-            socket.handler(this::dataHandler.curry(socket))
+        socket.handler(b -> this.dataHandler(socket, b));
     }
 
     void dataHandler(NetSocket socket, Buffer b) {
-        if (b.toString().matches(/[A-Za-z0-9 \n\r]*/)) {
-            socket.write("Hello ${b.toString()}".toString())
+        if (b.toString().matches("[A-Za-z0-9 \n\r]*")) {
+            socket.write("Hello "+b.toString());
         }
     }
 }

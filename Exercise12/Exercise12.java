bc012d77d1afab53c1637e2fe82dffa3d7a4a931
Deven Phillips <deven.phillips@gmail.com>
2017-05-26 12:23:35 -0400

    Updated Exercise 12

--- Exercise12/Exercise12.java
+++ Exercise12/Exercise12.java
@@ -1,3 +1,4 @@
+import io.vertx.core.http.HttpClient;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.http.HttpClientResponse;
@@ -5,20 +6,20 @@ import io.vertx.core.AbstractVerticle;
 
 public class Exercise12 extends AbstractVerticle {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Exercise12)
+    private static final Logger LOG = LoggerFactory.getLogger("Exercise12");
 
     @Override
     public void start() throws Exception {
-        def client = vertx.createHttpClient()
-                          .getNow('www.google.com', '/', this::responseHandler)
+        vertx.createHttpClient()
+                          .getNow("www.google.com", "/", this::responseHandler);
     }
 
     void responseHandler(HttpClientResponse response) {
-        if (response.statusCode()==200 && response.statusMessage()=='OK') {
-            LOG.info('Success!')
+        if (response.statusCode()==200 && response.statusMessage()=="OK") {
+            LOG.info("Success!");
         } else {
-            LOG.warn("Got ${response.statusCode()} as the response code.")
+            LOG.warn("Got ${response.statusCode()} as the response code.");
         }
-        vertx.close()
+        vertx.close();
     }
 }
--- Exercise12/Solution1.java
+++ Exercise12/Solution1.java
@@ -1,30 +1,33 @@
+import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.http.HttpClientResponse;
 import io.vertx.core.AbstractVerticle;
 
+import java.util.Base64;
+
 public class Solution1 extends AbstractVerticle {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Solution1)
+    private static final Logger LOG = LoggerFactory.getLogger("Solution1");
 
     @Override
     public void start() throws Exception {
-        def req = vertx.createHttpClient()
-                          .get('www.google.com', '/', this::responseHandler)
-        def base64key = Base64.encoder.encodeToString(new StringBuilder('username')
-                                                .append(":").append('password')
-                                                .toString().getBytes())
+        HttpClientRequest req = vertx.createHttpClient()
+                          .get("www.google.com", "/", this::responseHandler);
+        String base64key = Base64.getEncoder().encodeToString(new StringBuilder("username")
+                                                .append(":").append("password")
+                                                .toString().getBytes());
 
-        req.putHeader('Authorization', "Basic ${base64key}".toString())
-        req.end()
+        req.putHeader("Authorization", "Basic "+base64key);
+        req.end();
     }
 
     void responseHandler(HttpClientResponse response) {
-        if (response.statusCode()==200 && response.statusMessage()=='OK') {
-            LOG.info('Success!')
+        if (response.statusCode()==200 && response.statusMessage()=="OK") {
+            LOG.info("Success!");
         } else {
-            LOG.warn("Got ${response.statusCode()} as the response code.")
+            LOG.warn("Got ${response.statusCode()} as the response code.");
         }
-        vertx.close()
+        vertx.close();
     }
 }

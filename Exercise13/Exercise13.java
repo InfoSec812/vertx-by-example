4053485c457d903dffeee1f9158e3457dfbed5d6
Deven Phillips <deven.phillips@gmail.com>
2017-05-26 13:14:00 -0400

    Updated Exercise 13

--- Exercise13/Exercise13.java
+++ Exercise13/Exercise13.java
@@ -1,110 +1,118 @@
 import io.vertx.core.AsyncResult;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.core.AbstractVerticle;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+
 public class Exercise13 extends AbstractVerticle {
 
 
-    public static final Logger LOG = LoggerFactory.getLogger(Exercise13)
+    public static final Logger LOG = LoggerFactory.getLogger("Exercise13");
 
     @Override
     public void start() throws Exception {
-        Router router = Router.router(vertx)
+        Router router = Router.router(vertx);
 
-        router.route('/merged/').handler(this::reqHandler)
+        router.route("/merged/").handler(this::reqHandler);
 
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080, '0.0.0.0')
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080, "0.0.0.0");
     }
 
     void reqHandler(RoutingContext ctx) {
-        LOG.info('Request recieved')
-        def futureList = []
+        LOG.info("Request recieved");
+        List<Future> futureList = new ArrayList<>();
 
-        def googFuture = Future.future()
-        futureList.add(googFuture)
-        vertx.createHttpClient([ssl:true]).getNow(443, 'www.google.com', '/', this::httpClientResponseHandler.curry(googFuture))
+        Future<String> googFuture = Future.future();
+        futureList.add(googFuture);
+        vertx.createHttpClient(new HttpClientOptions(new JsonObject().put("ssl", true))).getNow(443, "www.google.com", "/", resp -> this.httpClientResponseHandler(googFuture, resp));
 
-        def fileFuture = Future.future()
-        futureList.add(fileFuture)
-        vertx.fileSystem().readFile("${System.getenv().PWD}/../.gitignore", fileFuture.completer())
+        Future<String> fileFuture = Future.future();
+        futureList.add(fileFuture);
+        vertx.fileSystem().readFile(System.getenv().get("PWD")+"/../.gitignore", res -> {
+            if (res.succeeded()) {
+                fileFuture.complete(res.result().toString());
+            } else {
+                fileFuture.fail(res.cause());
+            }
+        });
 
-        def dnsFuture = Future.future()
-        futureList.add(dnsFuture)
-        vertx.createDnsClient(53, '8.8.8.8').lookup4('www.google.com', dnsFuture.completer())
+        Future<String> dnsFuture = Future.future();
+        futureList.add(dnsFuture);
+        vertx.createDnsClient(53, "8.8.8.8").lookup4("www.google.com", dnsFuture.completer());
 
-        CompositeFuture.all(futureList).setHandler(this::resultHandler.curry(ctx))
+        CompositeFuture.all(futureList).setHandler(cf -> this.resultHandler(ctx, cf));
     }
 
     void httpClientResponseHandler(Future googFuture, HttpClientResponse res) {
-        LOG.info('HTTP Request response recieved.')
+        LOG.info("HTTP Request response recieved.");
         if (res.statusCode() == OK.code()) {
-            res.bodyHandler({ b -> googFuture.complete(b)})
+            res.bodyHandler(b -> googFuture.complete(b.toString()));
         } else {
-            LOG.error('HTTP request failed')
-            googFuture.fail(res.statusMessage())
+            LOG.error("HTTP request failed");
+            googFuture.fail(res.statusMessage());
         }
     }
 
     void resultHandler(RoutingContext ctx, AsyncResult<CompositeFuture> res) {
-        LOG.info('All futures resolved.')
+        LOG.info("All futures resolved.");
         if (res.failed()) {
-            LOG.error('One or more items failed.', res.cause())
+            LOG.error("One or more items failed.", res.cause());
             ctx.response().setStatusCode(INTERNAL_SERVER_ERROR.code())
                           .setStatusMessage(INTERNAL_SERVER_ERROR.reasonPhrase())
-                          .putHeader('Content-Type', 'text/plain')
-                          .end(res.cause().localizedMessage)
+                          .putHeader("Content-Type", "text/plain")
+                          .end(res.cause().getLocalizedMessage());
         } else {
-            LOG.info('All futures succeeded.')
-            def cf = res.result()
-            String html
-            String ipAddr
-            String fileContent
-            LOG.info("CompositeFuture Size: ${cf.size()}")
-            def range = cf.size() - 1
-            (0..range).each { idx ->
-                switch (cf.resultAt(idx)) {
-                    case { it -> (it.class.simpleName=='String') }: // DNS response
-                        LOG.info('DNS Parsed')
-                        ipAddr = cf.resultAt(idx)
-                        break
-                    case { it -> (it.class.simpleName=='Buffer' && ((Buffer)it).length()>2000) }:  // HTML response from Google
-                        LOG.info('HTML Parsed')
-                        def b = cf.resultAt(idx) as Buffer
-                        html = b.toString()
-                        break
-                    default:
-                        LOG.info('FILE Parsed')
-                        def b = cf.resultAt(idx) as Buffer
-                        fileContent = b.toString()
+            LOG.info("All futures succeeded.");
+            CompositeFuture cf = res.result();
+            String html = "";
+            String ipAddr = "";
+            String fileContent = "";
+            LOG.info("CompositeFuture Size: ${cf.size()}");
+            for (int x=0; x<cf.size(); x++) {
+                if (((String)cf.resultAt(x)).length()>2000) {
+                    html = cf.resultAt(x);
+                } else if (((String)cf.resultAt(x)).length()<30) {
+                    ipAddr = cf.resultAt(x);
+                } else {
+                    fileContent = cf.resultAt(x);
                 }
             }
-            LOG.info('All results stored')
+            LOG.info("All results stored");
 
-            def insert = "<pre>\n${ipAddr}\n\n${fileContent}</pre></body>".toString()
+            String insert = "<pre>"+fileContent+" - "+ipAddr+"</pre></body>";
 
-            LOG.info("Insert generated: ${insert}")
+            LOG.info("Insert generated: "+insert);
 
             try {
-                def matcher = (html =~ /<.body>/)
-                def output = matcher.replaceAll(insert)
+                Pattern p = Pattern.compile("</body>");
+                Matcher m = p.matcher(html);
+                String output = m.replaceAll(insert);
 
-                LOG.info('Output generated')
+                LOG.info("Output generated");
                 ctx.response().setStatusCode(OK.code())
                               .setStatusMessage(OK.reasonPhrase())
-                              .putHeader('Content-Type', 'text/html')
-                              .end(output)
+                              .putHeader("Content-Type", "text/html")
+                              .end(output);
             } catch (Throwable t) {
-                LOG.error('Something Bad!', t)
+                LOG.error("Something Bad!", t);
                 ctx.response().setStatusCode(INTERNAL_SERVER_ERROR.code())
                               .setStatusMessage(INTERNAL_SERVER_ERROR.reasonPhrase())
-                              .putHeader('Content-Type', 'text/plain')
-                              .end(t.localizedMessage)
+                              .putHeader("Content-Type", "text/plain")
+                              .end(t.getLocalizedMessage());
             }
         }
     }
--- Exercise13/Solution1.java
+++ Exercise13/Solution1.java
@@ -1,4 +1,6 @@
 import io.vertx.core.AsyncResult;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.CompositeFuture;
@@ -9,118 +11,123 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.core.AbstractVerticle;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static io.netty.handler.codec.http.HttpResponseStatus.INTERNAL_SERVER_ERROR;
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 
 public class Solution1 extends AbstractVerticle {
 
-    public static final Logger LOG = LoggerFactory.getLogger(Solution1)
-
-    def config = [
-        webHost: 'www.google.com',
-        webUri: '/',
-        webPort: 443,
-        webSSL: true,
-        hostname: 'www.reddit.com',
-        filename: System.getenv().PWD+'/Exercise1/Exercise1.groovy',
-        dnsServer: '8.8.8.8',
-        dnsPort: 53
-    ]
+    public static final Logger LOG = LoggerFactory.getLogger("Solution1");
+    private JsonObject config;
 
     @Override
     public void start() throws Exception {
-        Router router = Router.router(vertx)
+        if (vertx.getOrCreateContext().config().isEmpty()) {
+            config = new JsonObject()
+                            .put("webHost", "www.google.com")
+                            .put("webUri", "/")
+                            .put("webPort", 443)
+                            .put("webSSL", true)
+                            .put("hostname", "www.reddit.com")
+                            .put("filename", System.getenv().get("PWD")+"/Exercise1/Exercise1.java")
+                            .put("dnsServer", "8.8.8.8")
+                            .put("dnsPort", 53);
+        } else {
+            config = vertx.getOrCreateContext().config();
+        }
 
-        config += context.config()
+        Router router = Router.router(vertx);
 
-        router.route('/merged/').handler(this::reqHandler)
+        router.route("/merged/").handler(this::reqHandler);
 
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080, '0.0.0.0')
+        vertx.createHttpServer().requestHandler(router::accept).listen(8080, "0.0.0.0");
     }
 
     void reqHandler(RoutingContext ctx) {
-        LOG.info('Request recieved')
-        def futureList = []
+        LOG.info("Request recieved");
+        List<Future> futureList = new ArrayList<>();
 
-        def googFuture = Future.future()
-        futureList.add(googFuture)
-        vertx.createHttpClient([ssl:true]).getNow(config.webPort, config.webHost, config.webUri, this::httpClientResponseHandler.curry(googFuture))
+        Future<String> googFuture = Future.future();
+        futureList.add(googFuture);
+        vertx.createHttpClient(new HttpClientOptions()).getNow(443, config.getString("webHost"), config.getString("webUri"), resp -> this.httpClientResponseHandler(googFuture, resp));
 
-        def fileFuture = Future.future()
-        futureList.add(fileFuture)
-        vertx.fileSystem().readFile(config.filename, fileFuture.completer())
+        Future<String> fileFuture = Future.future();
+        futureList.add(fileFuture);
+        vertx.fileSystem().readFile(config.getString("filename"), res -> {
+            if (res.succeeded()) {
+                fileFuture.complete(res.result().toString());
+            } else {
+                fileFuture.fail(res.cause());
+            }
+        });
 
-        def dnsFuture = Future.future()
-        futureList.add(dnsFuture)
-        vertx.createDnsClient(config.dnsPort, config.dnsServer).lookup4(config.hostname, dnsFuture.completer())
+        Future<String> dnsFuture = Future.future();
+        futureList.add(dnsFuture);
+        vertx.createDnsClient(config.getInteger("dnsPort"), config.getString("dnsServer")).lookup4(config.getString("hostname"), dnsFuture.completer());
 
-        CompositeFuture.all(futureList).setHandler(this::resultHandler.curry(ctx))
+        CompositeFuture.all(futureList).setHandler(cf -> this.resultHandler(ctx, cf));
     }
 
     void httpClientResponseHandler(Future googFuture, HttpClientResponse res) {
-        LOG.info('HTTP Request response recieved.')
+        LOG.info("HTTP Request response recieved.");
         if (res.statusCode() == OK.code()) {
-            res.bodyHandler({ b -> googFuture.complete(b)})
+            res.bodyHandler(b -> googFuture.complete(b.toString()));
         } else {
-            LOG.error('HTTP request failed')
-            googFuture.fail(res.statusMessage())
+            LOG.error("HTTP request failed");
+            googFuture.fail(res.statusMessage());
         }
     }
 
     void resultHandler(RoutingContext ctx, AsyncResult<CompositeFuture> res) {
-        LOG.info('All futures resolved.')
+        LOG.info("All futures resolved.");
         if (res.failed()) {
-            LOG.error('One or more items failed.', res.cause())
+            LOG.error("One or more items failed.", res.cause());
             ctx.response().setStatusCode(INTERNAL_SERVER_ERROR.code())
-                          .setStatusMessage(INTERNAL_SERVER_ERROR.reasonPhrase())
-                          .putHeader('Content-Type', 'text/plain')
-                          .end(res.cause().localizedMessage)
+                    .setStatusMessage(INTERNAL_SERVER_ERROR.reasonPhrase())
+                    .putHeader("Content-Type", "text/plain")
+                    .end(res.cause().getLocalizedMessage());
         } else {
-            LOG.info('All futures succeeded.')
-            def cf = res.result()
-            String html
-            String ipAddr
-            String fileContent
-            LOG.info("CompositeFuture Size: ${cf.size()}")
-            def range = cf.size() - 1
-            (0..range).each { idx ->
-                switch (cf.resultAt(idx)) {
-                    case { it -> (it.class.simpleName=='String') }: // DNS response
-                        LOG.info('DNS Parsed')
-                        ipAddr = cf.resultAt(idx)
-                        break
-                    case { it -> (it.class.simpleName=='Buffer' && ((Buffer)it).length()>2000) }:  // HTML response from Google
-                        LOG.info('HTML Parsed')
-                        def b = cf.resultAt(idx) as Buffer
-                        html = b.toString()
-                        break
-                    default:
-                        LOG.info('FILE Parsed')
-                        def b = cf.resultAt(idx) as Buffer
-                        fileContent = b.toString()
+            LOG.info("All futures succeeded.");
+            CompositeFuture cf = res.result();
+            String html = "";
+            String ipAddr = "";
+            String fileContent = "";
+            LOG.info("CompositeFuture Size: ${cf.size()}");
+            for (int x=0; x<cf.size(); x++) {
+                if (((String)cf.resultAt(x)).length()>2000) {
+                    html = cf.resultAt(x);
+                } else if (((String)cf.resultAt(x)).length()<30) {
+                    ipAddr = cf.resultAt(x);
+                } else {
+                    fileContent = cf.resultAt(x);
                 }
             }
-            LOG.info('All results stored')
+            LOG.info("All results stored");
 
-            def insert = "<pre>\n${ipAddr}\n\n${fileContent}</pre></body>".toString()
+            String insert = "<pre>"+fileContent+" - "+ipAddr+"</pre></body>";
 
-            LOG.info("Insert generated: ${insert}")
+            LOG.info("Insert generated: "+insert);
 
             try {
-                def matcher = (html =~ /<.body>/)
-                def output = matcher.replaceAll(insert)
+                Pattern p = Pattern.compile("</body>");
+                Matcher m = p.matcher(html);
+                String output = m.replaceAll(insert);
 
-                LOG.info('Output generated')
+                LOG.info("Output generated");
                 ctx.response().setStatusCode(OK.code())
-                              .setStatusMessage(OK.reasonPhrase())
-                              .putHeader('Content-Type', 'text/html')
-                              .end(output)
+                        .setStatusMessage(OK.reasonPhrase())
+                        .putHeader("Content-Type", "text/html")
+                        .end(output);
             } catch (Throwable t) {
-                LOG.error('Something Bad!', t)
+                LOG.error("Something Bad!", t);
                 ctx.response().setStatusCode(INTERNAL_SERVER_ERROR.code())
-                              .setStatusMessage(INTERNAL_SERVER_ERROR.reasonPhrase())
-                              .putHeader('Content-Type', 'text/plain')
-                              .end(t.localizedMessage)
+                        .setStatusMessage(INTERNAL_SERVER_ERROR.reasonPhrase())
+                        .putHeader("Content-Type", "text/plain")
+                        .end(t.getLocalizedMessage());
             }
         }
     }

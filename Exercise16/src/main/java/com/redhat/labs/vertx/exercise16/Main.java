0fdc9003248ea775dc778ab796213247f82cbc08
Deven Phillips <deven.phillips@gmail.com>
2017-05-27 12:14:29 -0400

    Started adding hawkular exercise

---
+++ Exercise16/src/main/java/com/redhat/labs/vertx/exercise16/Main.java
@@ -0,0 +1,12 @@
+package com.redhat.labs.vertx.exercise16;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class Main extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+
+    }
+}
---
+++ Exercise16/src/test/groovy/com/redhat/labs/vertx/exercise16/MainSpec.groovy
@@ -0,0 +1,6 @@
+package com.redhat.labs.vertx.exercise16
+
+import spock.lang.Specification
+
+class MainSpec extends Specification {
+}
